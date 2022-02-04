fefadb9fe5e69a1c75fbe2d9b188e8455a12acdf
wipro-ai-consulting <98979216+wipro-ai-consulting@users.noreply.github.com>
2022-02-04 17:31:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from transformers import pipeline
 
 import streamlit as st
 
+from transformers import AutoTokenizer
+
  
 st.image('https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Wipro_Primary_Logo_Color_RGB.svg/240px-Wipro_Primary_Logo_Color_RGB.svg.png')
 st.title('Medical Affairs Use case: Can AI understand special words used by doctors? ')
@@ -11,13 +13,7 @@ st.write('Can the AI spot CHEMICAL Names, Diseases Names in any sentance?  Try u
 st.header('To try out, Enter any text below and presss Control + Enter on keyboard') 
 st.header('Also try out different AI brains. One of models can spot Diseases names! Select a brain on the drop down on left side of this screen.') 
 
-
-classifier = pipeline("sentiment-analysis", model="finiteautomata/bertweet-base-sentiment-analysis")
-
-result = classifier("I hate you")[0]
-
-st.write(result['label'])
-
+ 
 
 question_answerer = pipeline("question-answering")
 
@@ -26,4 +22,11 @@ context = r""" Extractive Question Answering is the task of extracting an answer
 result = question_answerer(question="What is extractive question answering?", context=context)
 st.write(result['answer'])
 
- 
+
+
+checkpoint = "distilbert-base-uncased-finetuned-sst-2-english"
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+
+sequence = "I've been waiting for a HuggingFace course my whole life."
+
+model_inputs = tokenizer(sequence)
