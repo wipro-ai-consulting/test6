6e52edac0cf991cba181bf2c35a58e015cc26bfe
wipro-ai-consulting <98979216+wipro-ai-consulting@users.noreply.github.com>
2022-02-04 15:41:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,6 @@
-import spacy_streamlit
-from pathlib import Path
-import srsly
-import importlib
-import random
+from transformers import pipeline
+
 import streamlit as st
-
-import spacy
-import en_core_sci_sm
-import en_ner_bc5cdr_md
-import en_ner_craft_md
-
-MODELS = srsly.read_json(Path(__file__).parent / "models.json")
-DEFAULT_MODEL = "en_core_web_sm"
-DEFAULT_TEXT = "Myeloid derived suppressor cells (MDSC) are immature  myeloid cells with immunosuppressive activity. They accumulate in tumor-bearing mice and humans with different types of cancer, including hepatocellular carcinoma (HCC). Spinal and bulbar muscular atrophy (SBMA) is an inherited motor neuron disease caused by the expansion of a polyglutamine tract within the androgen receptor (AR). SBMA can be caused by this easily. Amoxicillin/clavulanic acid, also known as co-amoxiclav or amox-clav, is an antibiotic medication used for the treatment of a number of bacterial infections. It is a combination consisting of amoxicillin, a β-lactam antibiotic, and potassium clavulanate, a β-lactamase inhibitor"
-DESCRIPTION = """** Customization by Wipro based on OpenAI scispacy**"""
 
  
 st.image('https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Wipro_Primary_Logo_Color_RGB.svg/240px-Wipro_Primary_Logo_Color_RGB.svg.png')
@@ -27,19 +14,10 @@ st.header('Also try out different AI brains. One of models can spot Diseases nam
 
 
 
-def get_default_text(nlp):
-    # Check if spaCy has built-in example texts for the language
-    try:
-        examples = importlib.import_module(f".lang.{nlp.lang}.examples", "spacy")
-        return DEFAULT_TEXT
-    except (ModuleNotFoundError, ImportError):
-        return ""
+unmasker2 = pipeline("fill-mask", model="microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract")
+fillinsentance = "adjuvant Vaccines induce [MASK] response"
 
-spacy_streamlit.visualize(
-    MODELS,
-    default_model=DEFAULT_MODEL,
-    visualizers=[ "ner"],
-    show_visualizer_select=True,
-    sidebar_description=DESCRIPTION,
-    get_default_text=get_default_text
-)
+resultsfillmask = unmasker(fillinsentance, top_k=4)
+
+for i in range(len(resultsfillmask)):
+    st.header (resultsfillmask[i]['sequence'])
